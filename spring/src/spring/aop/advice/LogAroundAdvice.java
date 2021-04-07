47a57e1c75734a3d7841da26cd970039464b1556
goodchuck <79144494+goodchuck@users.noreply.github.com>
2021-04-07 16:23:53 +0900

    21/04/07 AOP 7강 수강

--- spring/src/spring/aop/advice/LogAroundAdvice.java
+++ spring/src/spring/aop/advice/LogAroundAdvice.java
@@ -14,7 +14,7 @@ public class LogAroundAdvice implements MethodInterceptor{
 		
 		long end = System.currentTimeMillis();
 		String message = (end - start) + "ms 시간이 걸렸습니다.";
-		
+
 		System.out.println(message);
 		return result;
 	}
